7d2831a3640fc1cb949a847db67dbdf55160ce1d
Aziz <atamirzaev.aziz@mail.ru>
2022-05-25 14:04:32 +0600

    wrapped components in route

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import Header from "./components/Header";
-import Categories from "./components/Categories";
-import PizzaBlock from "./components/PizzaBlock";
-import Sort from "./components/Sort";
+import React from "react";
 
-import pizzas from './assets/pizzas.json'
+import {Route, Routes} from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
 
 import './scss/app.scss'
 
 function App() {
-
     return (
         <div className="wrapper">
             <Header/>
             <div className="content">
                 <div className="container">
-                    <div className="content__top">
-                        <Categories/>
-                        <Sort/>
-                    </div>
-                    <h2 className="content__title">Все пиццы</h2>
-                    <div className="content__items">
-                        {
-                            pizzas.map((obj) => (
-                                <PizzaBlock {...obj} key={obj.id}/>
-                            ))
-                        }
-                    </div>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/cart" element={<Cart/>}/>
+                        <Route path="*" element={<NotFound/>}/>
+                    </Routes>
                 </div>
             </div>
         </div>
-);
+    );
 }
 
 export default App;
