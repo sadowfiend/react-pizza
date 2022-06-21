cc8681d7d72bfe49abfb8a20acd80e116bbcf8c2
Aziz <atamirzaev.aziz@mail.ru>
2022-06-21 19:18:07 +0600

    used context

--- src/App.js
+++ src/App.js
@@ -9,19 +9,26 @@ import NotFound from "./pages/NotFound";
 
 import './scss/app.scss'
 
+export const SearchContext = React.createContext();
+
+console.log(SearchContext)
+
 function App() {
+    const [searchValue, setSearchValue] = React.useState('')
+
+    console.log(searchValue)
     return (
         <div className="wrapper">
-            <Header/>
-            <div className="content">
-                <div className="container">
+            <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+                <Header/>
+                <div className="content">
                     <Routes>
                         <Route path="/" element={<Home/>}/>
                         <Route path="/cart" element={<Cart/>}/>
                         <Route path="*" element={<NotFound/>}/>
                     </Routes>
                 </div>
-            </div>
+            </SearchContext.Provider>
         </div>
     );
 }
