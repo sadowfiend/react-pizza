dbb0e5a5971b794e714de5dfeef47c8ca8716887
Aziz <atamirzaev.aziz@mail.ru>
2022-06-21 19:15:24 +0600

    added states and filter to search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,37 +3,68 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import PizzaBlock from "../components/PizzaBlock";
+import Pagination from "../components/Pagination";
+import {SearchContext} from "../App";
 
-function Home(props) {
-    let [items, setItems] = React.useState([])
-    let [isLoading, setIsLoading] = React.useState(true)
+function Home() {
+    const [items, setItems] = React.useState([])
+    const [isLoading, setIsLoading] = React.useState(true)
+    const [categoryId, setCategoryId] = React.useState(0);
+    const [currentPage, setCurrentPage] = React.useState(1);
+    const [sortType, setSortType] = React.useState(
+        {name: 'популярности', sortProperty: 'rating'}
+    );
+
+    const {searchValue} = React.useContext(SearchContext);
+
+    const pizzas = items
+        .filter((obj) => {
+            if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+                return true;
+            }
+            return false;
+        })
+        .map((obj) => <PizzaBlock key={obj.id} {...obj}/>);
+    const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index}/>);
 
 
     React.useEffect(() => {
-        fetch('https://628bab00667aea3a3e344015.mockapi.io/items')
-            .then((res) => {
-                return res.json();
-            })
+        setIsLoading(true)
+
+        const sortBy = sortType.sortProperty.replace('-', '');
+        const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+        const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const search = searchValue ? `&search=${searchValue}` : '';
+
+        fetch(`https://628bab00667aea3a3e344015.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
+        )
+            .then((res) => res.json())
             .then((data) => {
                 setItems(data)
                 setIsLoading(false)
             })
-    }, [])
+        window.scrollTo(0,0);
+    }, [categoryId, sortType, searchValue, currentPage])
 
     return (
-        <>
+        <div className="container">
             <div className="content__top">
-                <Categories/>
-                <Sort/>
+                <Categories value={categoryId}
+                            onChangeCategory={(i) => setCategoryId(i)}
+                />
+                <Sort value={sortType}
+                      onChangeSort={(i) => setSortType(i)}
+                />
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
                 {isLoading
-                    ? [...new Array(6)].map((_, index) => <Skeleton key={index}/>)
-                    : items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>)
+                    ? skeletons
+                    : pizzas
                 }
             </div>
-        </>
+            <Pagination onChangePage={(number) => setCurrentPage(number)}/>
+        </div>
     );
 }
 
